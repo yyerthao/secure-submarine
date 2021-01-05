45a1d853c114bfba59ee191911b618805655cf38
Yer Thao <maivyerthao@gmail.com>
2021-01-05 17:19:00 -0600

    Updated secrets.router.js, ongoing progress

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
-router.get('/', (req, res) => {
+
+router.get('/', rejectUnauthenticated, (req, res) => {
   console.log('req.user:', req.user);
+  console.log('is authenticated?', req.isAuthenticated());
+
   pool
-    .query('SELECT * FROM "secret";')
+    .query('SELECT * FROM "secret" WHERE "secrecy_level" < $13;', [req.user.id])
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log('Error making SELECT for secrets:', error);
-      res.sendStatus(500);
+      res.sendStatus(403);
     });
 });
 
